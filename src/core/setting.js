2b06954694a8fd1582c7cf415aad025126443e96
icyzeroice <ziv3@outlook.com>
2018-08-11 22:57:31 +0800

    TEMP SAVE: keynote position ready

--- src/core/setting.js
+++ src/core/setting.js
@@ -4,7 +4,10 @@ export default {
   
   WINDOW_RESIZE_TIMEOUT: 200,
 
-  SLIDE_CLASS: '.keynote-slide',
+  CONTAINER_CLASS: '.keynote',
+  SLIDE_CLASS: '.keynote__slide',
+  SLIDE_CONTENT_CLASS: '.keynote__slide__content',
+  
   SLIDE_HEIGHT: 540, // px
   SLIDE_WIDTH: 960,  // px
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import Keynote from './core/keynote.js'
 import './style/index.scss'
 
-const app = new Keynote('.keynote')
+const app = new Keynote('.MyPPT')
 app.play()
+// app.overview()
